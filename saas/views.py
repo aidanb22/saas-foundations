9c66da37b8be7721f4a7e1c8f5acfaf9974078a9
ablancas707 <ablancas@trinity.edu>
2024-10-26 19:04:50 -0500

    updated views

--- saas/views.py
+++ saas/views.py
@@ -6,25 +6,19 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def nozzle():
-    pass
-
-
 def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     """Home Page View returns a HTML  """
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "percent": page_qs.count() * 100.0 / qs.count(),
         "total_visit_count": qs.count(),
     }
-    path = request.path
-    print("path", path)
     PageVisit.objects.create(path=request.path)
-    html_template = "home.html"
     return render(request, html_template, my_context)
 
 
